3112b7b5c144653874bce3bfd5e68f4220827fc3
Robert HC <bj112143@gmail.com>
2018-01-30 11:41:01 -0500

    First upload - with auth

---
+++ WebApiRest/WebApiRest/Controllers/AutenticadoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Security;
+
+namespace WebApiRest.Controllers
+{
+    public class AutenticadoController : ApiController
+    {
+        public string Get(string id)
+        {
+            FormsAuthentication.SetAuthCookie(id ?? "FooUser", false);
+
+            return "You are authenticated now";
+        }
+    }
+}
---
+++ WebApiRest/WebApiRest/Filters/BasicAuthorizeAttribute.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace WebApiRest.Filters
+{
+    public class BasicAuthorizeAttribute : AuthorizationFilterAttribute
+    {
+        public override void OnAuthorization(HttpActionContext actionContext)
+
+        {
+
+            var headers = actionContext.Request.Headers;
+
+            if (headers.Authorization != null && headers.Authorization.Scheme == "Basic")
+
+            {
+
+                try
+
+                {
+
+                    var userPwd = Encoding.UTF8.GetString(Convert.FromBase64String(headers.Authorization.Parameter));
+
+                    var user = userPwd.Substring(0, userPwd.IndexOf(':'));
+
+                    var password = userPwd.Substring(userPwd.IndexOf(':') + 1);
+
+                    // Validamos user y password (aquí asumimos que siempre son ok)
+
+                }
+
+                catch (Exception)
+
+                {
+
+                    PutUnauthorizedResult(actionContext, "Invalid Authorization header");
+
+                }
+
+            }
+
+            else
+
+            {
+
+                // No hay el header Authorization
+
+                PutUnauthorizedResult(actionContext, "Auhtorization needed");
+
+            }
+
+        }
+
+
+
+        private void PutUnauthorizedResult(HttpActionContext actionContext, string msg)
+        {
+
+            actionContext.Response = new HttpResponseMessage(HttpStatusCode.Unauthorized)
+            {
+
+                Content = new StringContent(msg)
+
+            };
+
+        }
+    }
+}
